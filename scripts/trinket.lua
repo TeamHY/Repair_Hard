c20b610cb27ae8888874d1315e49bba294099d4b
PowerNao <powernao@gmail.com>
2020-10-19 18:34:09 +0900

    아오갓 픽스
    TEAR_GLOW 초기화 추가 및 갓헤드 예외 추가

--- scripts/trinket.lua
+++ scripts/trinket.lua
@@ -99,7 +99,8 @@ function RepairMod:ShootTear(tear)
 			tear.TearFlags = tear.TearFlags|TearFlags.TEAR_HOMING
 		end
 	end
-	if player:HasTrinket(eye_of_god) then
+	if player:HasTrinket(eye_of_god) and player:HasCollectible(331) == false then
+		player.TearFlags = player.TearFlags&~TearFlags.TEAR_GLOW
 		if player:GetTrinketRNG(eye_of_god):RandomInt(20) < (5 + player.Luck) then --0부터 9 / 1 + 럭 미만
 			player.TearFlags = player.TearFlags|TearFlags.TEAR_GLOW
 		end
